91c53f0806b3d98392b251a53966829f74710e0b
Renat Mansurov <wiredrat@protonmail.com>
2020-02-03 20:58:21 +0300

    Changed directory structure

---
+++ static/js/script.js
@@ -0,0 +1,22 @@
+function fetch_post(card_id, route_url) {
+    fetch('/' + route_url + '/' + card_id, {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        data: JSON.stringify(card_id)
+    }).then(response => console.log(response));
+}
+
+function change_status(event) {
+    let card_id = event.target.parentNode.parentNode.parentNode.id;
+    fetch_post(card_id, 'change');
+}
+
+function delete_task(event) {
+    let card_id = event.target.parentNode.parentNode.parentNode.id;
+    fetch_post(card_id, 'delete')
+}
+
+function showForm() {
+    let form = document.querySelector('form');
+    form.style.display = (form.style.display == 'none') ? 'block' : 'none';
+}
